3b12cd30701463cc100c07f68fb739b10fbb0e4b
xingruic <xingruichen2@gmail.com>
2024-10-28 18:20:41 -0700

    clear everything

--- include/xrc/usercontrol.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace xrobot{
-    void handle_controller_input();
-}
--- src/xrc/utils.cpp
+++
@@ -1,9 +0,0 @@
-#include "xrc/utils.hpp"
-
-double deg2rad(double degrees){
-    return degrees*M_PI/180;
-}
-
-double dsin(double degrees){
-    return sin(deg2rad(degrees));
-}
