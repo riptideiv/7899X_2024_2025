729774aa30824b5fd60d924d5e0b29351957f515
7899VRC <138609260+7899VRC@users.noreply.github.com>
2025-01-24 17:47:29 -0800

    added templates for future functions

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,12 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
+
 #include "vex.h"
 
+
 using namespace vex;
+
 
 // A global instance of competition
 competition Competition;
@@ -29,11 +32,13 @@ double pie = 3.14159;
 double dia = 2.75;
 double g = 1.0 / 1.0;
 
+
 vex::digital_out intakeupper(Brain.ThreeWirePort.A);
 vex::digital_out mogoClamp(Brain.ThreeWirePort.B);
 vex::digital_out Rdoinker(Brain.ThreeWirePort.C);
 vex::digital_out Ldoinker(Brain.ThreeWirePort.D);
 inertial Inertial = inertial(PORT2);
+
 
 void DriveVolts(double lspeed, double rspeed, double multiplier, int wt)
 {
@@ -48,6 +53,7 @@ void DriveVolts(double lspeed, double rspeed, double multiplier, int wt)
   task::sleep(wt);
 }
 
+
 void Coast1()
 {
   RB.stop(coast);
@@ -58,6 +64,7 @@ void Coast1()
   LM.stop(coast);
 }
 
+
 void Brake1()
 {
   RB.stop(brake);
@@ -67,6 +74,7 @@ void Brake1()
   LF.stop(brake);
   LM.stop(brake);
 }
+
 
 void inchDrive(float target, int timeout = 1500, float kp = 1.6)
 {
@@ -87,6 +95,7 @@ void inchDrive(float target, int timeout = 1500, float kp = 1.6)
   {
     x = RF.position(rev) * pie * dia * g;
     error = target - x;
+
 
     if (fabs(error) < tolerance)
     {
@@ -120,6 +129,7 @@ void gyroturnAbs(double target, int timeout = 1500)
   float accuracy = 0.1;
   float bias = 0;
 
+
   while (t1.time(msec) < timeout)
   {
     heading = Inertial.rotation(degrees);
@@ -135,28 +145,28 @@ void gyroturnAbs(double target, int timeout = 1500)
       integral = 0;
     }
 
+
     speed = kp * error + kd * derivative + ki * integral;
     DriveVolts(-speed, speed, 1, 0);
-    Controller1.Screen.setCursor(1, 1);
-    Controller1.Screen.clearLine();
-    Controller1.Screen.print(error);
   }
   Coast1();
   wait(10, msec);
 }
 
+
 void intakeSpinF(double time, double speed)
 {
   intake.spin(forward, speed * 120, voltageUnits::mV);
   wait(time, msec);
-  intake.stop();
 }
+
+
 void intakeSpinR(double time, double speed)
 {
   intake.spin(reverse, speed * 120, voltageUnits::mV);
   wait(time, msec);
-  intake.stop();
 }
+
 
 void bigArmRotateF(double time, double speed)
 {
@@ -165,12 +175,14 @@ void bigArmRotateF(double time, double speed)
   bigArm.stop();
 }
 
+
 void bigArmRotateR(double time, double speed)
 {
   bigArm.spin(reverse, speed * 120, voltageUnits::mV);
   wait(time, msec);
   bigArm.stop();
 }
+
 
 int bigArmTarget = 350;
 int bigArmPIDLoop(){
@@ -180,7 +192,6 @@ int bigArmPIDLoop(){
   }
   return 0;
 }
-
 // void resetBigArm () {
 //   BigArm.setRotation(0, degrees);
 //   double BigArmPos = BigArm.rotation(degrees);
@@ -191,7 +202,9 @@ int bigArmPIDLoop(){
 //   BigArmrotate(0);
 // }
 
+
 // define your global instances of motors and other devices here
+
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -203,20 +216,21 @@ int bigArmPIDLoop(){
 /*  not every time that the robot is disabled.                               */
 /*---------------------------------------------------------------------------*/
 
+
 void pre_auton(void)
 {
   // make something for the gyro to calibrate at a predetermined angle
   // print gyro value to brain
   Inertial.calibrate();
   while (true) {
-    int gyroReading = Inertial.value(); 
-    Brain.Screen.print("Gyro: ", gyroReading); 
+    int gyroReading = Inertial.value();
+    Brain.Screen.print("Gyro: ", gyroReading);
     wait(20, msec);
-  }
-
-  task bigArmPIDTask(bigArmPIDLoop);
+}
   // rotate the robot to find the starting pos degree
   // set a starting degree to startingpos degree so that the robot's turns are not weird
+
+
 
 
   // Inertial.resetRotation();
@@ -224,6 +238,7 @@ void pre_auton(void)
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
 }
+
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
@@ -235,21 +250,177 @@ void pre_auton(void)
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
+
 void autonomous(void)
-{ 
+{
   wait(200, msec);
+  // step 1 (alliance stake)
   bigArmRotateR(900, 100);
   wait(400,msec);
   inchDrive(-25,900);
+  // step 2 (first mogo)
   mogoClamp.set(true);
-  bigArmRotateF(900,100);
   bigArmRotateF(900,100);
   wait(400,msec);
   gyroturnAbs(-80,300);
-  intakeSpinF(0,100);
+  // step 3 (first floor ring)
+  intakeSpinF(2000,100);
   inchDrive(20,800);
-
+  // ----------- STARTING TEMPLATE ----------------
+  gyroturnAbs();
+  // step 4
+  intakeSpinF();
+  inchDrive();
+  intake.stop();
+  // step 5
+  gyroTurnAbs();
+  intakeSpinF();
+  inchDrive()
+  intake.stop();
+  // step 6 (THIS RING GOES INTO THE WALL STAKE)
+  bigArmRotateF(); // rotate up just a little bit just so that the ring goes into the LB instead of mogo
+  intakeSpinF();
+  inchDrive();
+  intake.stop();
+  // step 7 & 8 (a little diff cuz we intake at the same time as we put LB ring on wall stake)
+  gyroTurnAbs();
+  inchDrive();
+  gyroTurnAbs(); // idk if we need this im guestimating
+  inchDrive();
+  // spin intake & score on wall stake at same time
+  intakeSpinF(); // dont spin into mogo yet until the big arm finishes on wall stake
+  bigArmRotateF(); // LB onto wall stake
+  inchDrive() // drive backwards to get off the wall stake
+  bigArmRotateR(); // rotate back down to position to put in the ring in the intake
+  intakeSpinF(); // spin ring into LB
+  // MAY NEED to move forward (idk deepends where the robot ends up)
+  bigArmRotateF(); // score onto the wall stake
+  inchDrive(); // drive backwards off the wall stake
+  bigArmRotateR(); // set to LOWEST position
+  // step 9 (the 3 rings in the top right corner)
+  gyroTurnAbs(); // turn left
+  intakeSpinF();
+  inchDrive();
+  // step 10
+  gyroTurnAbs();
+  inchDrive();
+  // step 11
+  gyroTurnAbs();
+  inchDrive();
+  intake.stop();
+  // step 12
+  gyroTurnAbs(); // turn so the mogo clamp is facing the corner
+  inchDrive(); // drive backwards with mogo clamp to corner
+  mogoClamp.set(false); // drop off mogo
+  // step 13
+  gyroTurnAbs();
+  inchDrive();
+  gyroTurnAbs(); // now turn again so that the mogo clamp faces the middle mogo out of the 3
+  inchDrive();
+  mogoClamp.set(True); // clamp onto mogo
+  // step 14
+  gyroTurnAbs(); // turn so that intake is facing the next closest red ring
+  intakeSpinF();
+  inchDrive();
+  // step 15
+  gyroTurnAbs(); // may or may not need depending on where u end up
+  inchDrive();
+  intake.stop();
+  // step 16
+  gyroTurnAbs(); // get ring that is reflected over the double white line
+  intakeSpinF();
+  inchDrive();
+  // step 17 (get the two rings)
+  inchDrive();
+  // step 18
+  gyroTurnAbs();
+  intakeSpinF();
+  inchDrive();
+  intake.stop();
+  // step 19
+  gyroTurnAbs(); // turn so that the mogo faces the corner
+  inchDrive(); // drive backwards to put mogo goal in corner
+  mogoClamp.set(False); // drop mogo in corner
+  // step 20 (GOES INTO LB FOR WALL STAKE)
+  gyroTurnAbs(); // may or may not need depending on where u end up (agian lol)
+  bigArmRotateF(); // rotate just enough for the ring to go into the LB instead of mogo
+  intakeSpinF();
+  inchDrive();
+  intake.stop();
+  // step 21 AND 22 (this stuff is probably all over the place and needs to be changed alot)
+  gyroTurnAbs();
+  inchDrive();
+    // now spin the intake and big arm at the same time
+  bigArmRotateF(); // score on the wall stake
+  intakeSpinF(); // don't spin all the way up (wait for the big arm to go back down before u spin more)
+  inchDrive(); // drive backwards off the wall stkae
+  bigArmRotateR(); // rotate back down so that the ring in the intake can go into the LB
+  intakeSpinF(); // spin ring into the LB
+  bigArmRotateF(); // score on the wall stake
+  inchDrive(); // drive backwards to get off the wall stake
+  bigArmRotateR(); // rotate LB all the way down to lowest pos
+  // step 23 (this ring goes into LB)
+  gyroTurnAbs();
+  inchDrive();
+  bigArmRotateF(); // rotate LB up just a little but so that the ring goes into the LB
+  gyroTurnAbs();
+  intakeSpinF(); // intake into LB
+  inchDrive();
+  intake.stop();
+  // step 24
+  gyroTurnAbs();
+  inchDrive();
+  gyroTurnAbs();
+  inchDrive(); // stop just in front of the alliance wall stake
+  bigArmRotateF(); // turn LB onto wall stake
+  inchDrive(); // drive backward to get off the wall stake
+  bigArmRotateR(); // rotate LB back to lowest pos
+  // step 25
+  gyroTurnAbs(); // rotate bot so that the mogo clamp is facing the mogo with blue ring on it
+  inchDrive(); // drive backward to get the mogo
+  mogoClamp.set(True);
+  // step 26 (SAVE THESE TWO RINGS FOR THE OTHER MOGO (step 28 ish))
+  gyroTurnAbs(); // do a 180 turn so that the front intake faces the other rings
+  intakeSpinF(); // only run it a little bit cuz it needs to stay in the intake
+  inchDrive(); // drive forward to get the ring
+  intake.stop();
+  gyroTurnAbs(); // turn to get the other ring next to it
+  intakeSpinF(); // also only run a little bit
+  inchDrive();
+  intake.stop();
+  // step 27
+  gyroTurnAbs(); // turn so that the clamp faces the corner
+  inchDrive(); // drive backwards into the corner
+  mogoClamp.set(False);
+  // step 28
+  gyroTurnAbs(); // turn to the direction to get the last mogo
+  inchDrive();
+  gyroTurnAbs(); // once u get to a close enough pos, turn so that mogo clamp faces the mogo
+  inchDrive(); // a little
+  mogoClamp.set(True);
+  intakeSpinF(); // spin enough so that the rings that were previously stored go onto the mogo
+  intake.stop();
+  // step 29
+  gyroTurnAbs();
+  intakeSpinF();
+  inchDrive();
+  // step 30
+  gyroTurnAbs();
+  inchDrive();
+  // step 31
+  gyroTurnAbs();
+  inchDrive();
+  intake.stop();
+  // step 32
+  gyroTurnAbs(); // turn so mogo clamp faces the corner
+  inchDrive(); // drive backwards into mogo
+  mogoClamp.set(False);
+  // step 33
+  gyroTurnAbs(); // rotate to prep drive to the hang
+    // include whaterver we are using to hang
+  inchDrive();
 }
+
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
@@ -261,6 +432,7 @@ void autonomous(void)
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
+
 void usercontrol(void)
 {
   // User control code here, inside the loop
@@ -270,15 +442,18 @@ void usercontrol(void)
     // Each time through the loop your program should update motor + servo
     // values based on feedback from the joysticks.
 
+
     // ........................................................................
     // Insert user code here. This is where you use the joystick values to
     // update your motors, etc.
     // ........................................................................
 
+
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
   }
 }
+
 
 //
 // Main will set up the competition functions and callbacks.
@@ -289,8 +464,10 @@ int main()
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
 
+
   // Run the pre-autonomous function.
   pre_auton();
+
 
   // Prevent main from exiting with an infinite loop.
   while (true)
@@ -298,3 +475,4 @@ int main()
     wait(100, msec);
   }
 }
+
